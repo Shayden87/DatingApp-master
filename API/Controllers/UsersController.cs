1580fab06262771f74c803aa80b86875e62e714c
Spencer Hayden <75949516+Shayden87@users.noreply.github.com>
2022-05-03 08:58:57 -0700

    Photo Management

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -65,7 +65,10 @@ namespace API.Controllers
         [HttpGet("{username}", Name = "GetUser")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
-            return await _unitOfWork.UserRepository.GetMemberAsync(username);
+            var CurrentUsername = User.GetUsername();
+            return await _unitOfWork.UserRepository.GetMemberAsync(
+                    username, isCurrentUser: CurrentUsername == username
+            );
         }
 
         [HttpPut]
@@ -96,11 +99,6 @@ namespace API.Controllers
                 Url = result.SecureUrl.AbsoluteUri,
                 PublicId = result.PublicId
             };
-
-            if (user.Photos.Count == 0)
-            {
-                photo.IsMain = true;
-            }
 
             user.Photos.Add(photo);
 
